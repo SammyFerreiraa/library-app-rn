8ba4800eb0a8dfa86200747bb8dfd6f432a917b1
Sammy Ferreira <sammyfe2021@gmail.com>
2024-02-13 12:13:41 -0300

    :sparkles: feat: SafeAreaView

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,13 @@
 import Routes from '@/routes'
 import { NavigationContainer } from '@react-navigation/native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Layout() {
   return (
-    <NavigationContainer independent={true}>
-      <Routes />
-    </NavigationContainer>
+    <SafeAreaView className="flex-1" style={{ backgroundColor: 'black' }}>
+      <NavigationContainer independent={true}>
+        <Routes />
+      </NavigationContainer>
+    </SafeAreaView>
   )
 }
