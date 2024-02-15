ac5050d01a53ba4238574c7a557ef312ad874f71
Sammy Ferreira <sammyfe2021@gmail.com>
2024-02-15 20:06:02 -0300

    :recycle: feat: useBook hook

---
+++ src/hooks/useBook.ts
@@ -0,0 +1,20 @@
+import { create } from 'zustand'
+
+export type bookProps = {
+  id: string
+  title: string
+  author: string
+  isbn: string
+  delays: number
+  image: string
+  recommended: boolean
+}
+interface useBookProps {
+  books: bookProps[]
+  setBooks: (books: bookProps[]) => void
+}
+
+export const useBook = create<useBookProps>((set) => ({
+  books: [],
+  setBooks: (books) => set({ books }),
+}))
