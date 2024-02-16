3e9d32da57c195ecba854d63e4cf596349d95be4
Sammy Ferreira <sammyfe2021@gmail.com>
2024-02-16 15:34:37 -0300

    :sparkles: feat: sign out functionality to banner component

--- src/screens/home/banner-home.tsx
+++ src/screens/home/banner-home.tsx
@@ -1,14 +1,16 @@
+import { useAuth } from '@/hooks/useAuth'
 import React from 'react'
 import { View, Text } from 'react-native'
 
 export const Banner = () => {
+  const { signOut } = useAuth()
   return (
     <View className="mx-4 flex-col rounded-xl bg-[#2d3047] p-4">
       <Text className="mb-6 text-2xl font-bold text-white">
         Ganhe acesso ilimitado a todos os livros por apenas
       </Text>
       <Text className="mb-10 text-5xl font-bold text-white">R$10,99</Text>
-      <Text className="font-thin text-white">
+      <Text className="font-thin text-white" onPress={signOut}>
         *Termos e condições aplicados
       </Text>
     </View>
