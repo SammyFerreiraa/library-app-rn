76df3c0589da8a94941770480b7f1bf27ed9819c
Sammy Ferreira <sammyfe2021@gmail.com>
2024-02-16 15:21:29 -0300

    :sparkles: feat: suffleArray

---
+++ src/utils/functions/shuffleArray.ts
@@ -0,0 +1,9 @@
+import { bookProps } from '@/hooks/useBook'
+
+export function shuffleArray(arr: bookProps[]) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[arr[i], arr[j]] = [arr[j], arr[i]]
+  }
+  return arr
+}
