6a4410091ba6c2bd2070e793d9db65815dd88447
Sammy Ferreira <sammyfe2021@gmail.com>
2024-02-16 15:24:49 -0300

    :sparkles: feat: asyncStorage books

--- src/hooks/useBook.ts
+++ src/hooks/useBook.ts
@@ -1,4 +1,6 @@
 import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export type bookProps = {
   id: string
@@ -15,7 +17,15 @@ interface useBookProps {
   setBooks: (books: bookProps[]) => void
 }
 
-export const useBook = create<useBookProps>((set) => ({
-  books: [],
-  setBooks: (books) => set({ books }),
-}))
+export const useBook = create(
+  persist<useBookProps>(
+    (set) => ({
+      books: [],
+      setBooks: (books) => set({ books }),
+    }),
+    {
+      name: 'books',
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+)
