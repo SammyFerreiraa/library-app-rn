7044310367c20c7837aee0dd56f1802088a91e74
Sammy Ferreira <sammyfe2021@gmail.com>
2024-02-17 10:42:07 -0300

    :recycle: refactor: image height in category screen

--- src/modules/Explorer/screens/categories/[category].tsx
+++ src/modules/Explorer/screens/categories/[category].tsx
@@ -47,7 +47,7 @@ export default function Categories() {
                     resizeMode="cover"
                     style={{
                       width: '100%',
-                      height: 256,
+                      height: 270,
                       marginBottom: 16,
                     }}
                   />
